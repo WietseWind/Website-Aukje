6efd89d7f5f01d7f419f8b6f1518bf3d2b6da058
Wietse Wind <mail@wietse.com>
2019-11-04 13:25:48 +0100

    Pre-live

---
+++ src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home">
+    <h2 class="mt-3">Privé-uitlaatservice</h2>
+    <h5 class="">Zeist en omstreken</h5>
+    <h2 class=""><strong><q>De Zesvoeter</q></strong></h2>
+    <h4>Aukje Wind</h4>
+    <p class="pt-3">
+      <a href="https://wa.me/31648077493" class="btn btn-md btn-success">
+        Contact via <i class="fab fa-whatsapp"></i> WhatsApp
+      </a>
+    </p>
+    <br />
+    <img alt="De Zesvoeter" src="../assets/aukje-de-zesvoeter.jpeg" class="img-thumbnail rounded aukje">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home'
+}
+</script>
+
+<style lang="scss" scoped>
+  img.aukje {
+    max-height: 60vh;
+    max-width: 80vw;
+  }
+</style>
