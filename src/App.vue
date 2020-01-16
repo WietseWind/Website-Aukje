1193c1859c86bfe987c5ea47dda485bf740a7ca9
Wietse Wind <mail@wietse.com>
2020-01-16 17:52:02 +0100

    Menu responsive

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <template>
   <div id="app" class="container">
-    <div id="nav" class="btn-group">
-      <router-link class="btn" :class="{ 'btn-outline-primary': $router.currentRoute.name !== 'home', 'btn-primary': $router.currentRoute.name === 'home' }" to="/">Home</router-link>
-      <router-link class="btn" :class="{ 'btn-outline-primary': $router.currentRoute.name !== 'visie', 'btn-primary': $router.currentRoute.name === 'visie' }" to="/visie">Visie</router-link>
-      <router-link class="btn" :class="{ 'btn-outline-primary': $router.currentRoute.name !== 'werkwijze', 'btn-primary': $router.currentRoute.name === 'werkwijze' }" to="/werkwijze">Werkwijze</router-link>
-      <router-link class="btn" :class="{ 'btn-outline-primary': $router.currentRoute.name !== 'over-mij', 'btn-primary': $router.currentRoute.name === 'over-mij' }" to="/over-mij">Over mij</router-link>
-      <router-link class="btn" :class="{ 'btn-outline-primary': $router.currentRoute.name !== 'wandelvoorwaarden', 'btn-primary': $router.currentRoute.name === 'wandelvoorwaarden' }" to="/wandelvoorwaarden">Wandelvoorwaarden</router-link>
-      <router-link class="btn" :class="{ 'btn-outline-primary': $router.currentRoute.name !== 'tarieven', 'btn-primary': $router.currentRoute.name === 'tarieven' }" to="/tarieven">Tarieven</router-link>
-      <router-link class="btn" :class="{ 'btn-outline-primary': $router.currentRoute.name !== 'contact', 'btn-primary': $router.currentRoute.name === 'contact' }" to="/contact">Contact</router-link>
+    <div id="nav" class="flex-wrap">
+      <router-link class="btn mx-1 my-1" :class="{ 'btn-outline-primary': $router.currentRoute.name !== 'home', 'btn-primary': $router.currentRoute.name === 'home' }" to="/">Home</router-link>
+      <router-link class="btn mx-1 my-1" :class="{ 'btn-outline-primary': $router.currentRoute.name !== 'visie', 'btn-primary': $router.currentRoute.name === 'visie' }" to="/visie">Visie</router-link>
+      <router-link class="btn mx-1 my-1" :class="{ 'btn-outline-primary': $router.currentRoute.name !== 'werkwijze', 'btn-primary': $router.currentRoute.name === 'werkwijze' }" to="/werkwijze">Werkwijze</router-link>
+      <router-link class="btn mx-1 my-1" :class="{ 'btn-outline-primary': $router.currentRoute.name !== 'over-mij', 'btn-primary': $router.currentRoute.name === 'over-mij' }" to="/over-mij">Over mij</router-link>
+      <router-link class="btn mx-1 my-1" :class="{ 'btn-outline-primary': $router.currentRoute.name !== 'wandelvoorwaarden', 'btn-primary': $router.currentRoute.name === 'wandelvoorwaarden' }" to="/wandelvoorwaarden">Wandelvoorwaarden</router-link>
+      <router-link class="btn mx-1 my-1" :class="{ 'btn-outline-primary': $router.currentRoute.name !== 'tarieven', 'btn-primary': $router.currentRoute.name === 'tarieven' }" to="/tarieven">Tarieven</router-link>
+      <router-link class="btn mx-1 my-1" :class="{ 'btn-outline-primary': $router.currentRoute.name !== 'contact', 'btn-primary': $router.currentRoute.name === 'contact' }" to="/contact">Contact</router-link>
     </div>
     <router-view/>
   </div>
