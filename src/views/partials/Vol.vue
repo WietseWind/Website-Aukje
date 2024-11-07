a2acfcb08e0b2865e2f48ca4c24c7df6c51038d5
Wietse Wind <mail@wietse.com>
2024-11-07 02:31:33 +0100

    Vol

--- src/views/partials/Vol.vue
+++ src/views/partials/Vol.vue
@@ -1,6 +1,6 @@
 <template>
   <div section="full" class="alert alert-warning px-3 py-2 mt-3 text-center">
-    Alle dagen zitten momenteel <b>vol</b>.
+    Alle dagen zitten momenteel <b>vol</b>.<br />
     We hebben een <b>wachtlijst</b>. Om je vrijblijvend aan te melden op de wachtlijst kun je
     <a target="_blank" href="https://docs.google.com/forms/d/e/1FAIpQLSct_IpcN59OuOSbgsmzjJcDF3YnNI438fG_NkxTZX1pF0c0sw/viewform">dit formulier</a>
     invullen.
